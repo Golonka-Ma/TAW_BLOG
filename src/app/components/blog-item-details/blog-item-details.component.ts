bb5470366ffb7af2d6e72e0f3cc05cda31a5c942
Golonka-Ma <115418615+Golonka-Ma@users.noreply.github.com>
2024-06-25 16:13:05 +0200

    Added blog details and back button font fix

--- src/app/components/blog-item-details/blog-item-details.component.ts
+++ src/app/components/blog-item-details/blog-item-details.component.ts
@@ -1,7 +1,7 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {DataService} from "../../services/data.service";
-import {HttpClientModule} from "@angular/common/http";
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DataService } from "../../services/data.service";
+import { HttpClientModule } from "@angular/common/http";
 
 @Component({
   selector: 'app-blog-item-details',
@@ -9,30 +9,29 @@ import {HttpClientModule} from "@angular/common/http";
   imports: [HttpClientModule],
   providers: [DataService],
   templateUrl: './blog-item-details.component.html',
-  styleUrl: './blog-item-details.component.css'
+  styleUrls: ['./blog-item-details.component.css']
 })
 export class BlogItemDetailsComponent implements OnInit {
   public image: string = '';
   public text: string = '';
 
-  constructor(private service: DataService, private route: ActivatedRoute) {
-  }
+  constructor(private service: DataService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     let id: string = '';
-    this.route.paramMap
-      .subscribe((params: any) => {
-        id = params.get('id');
-
-      });
+    this.route.paramMap.subscribe((params: any) => {
+      id = params.get('id');
+    });
 
     this.service.getById(id).subscribe((res: any) => {
-
       console.log(res);
       const post = res;
       this.image = post['image'];
       this.text = post['text'];
     });
+  }
 
+  goBack() {
+    this.router.navigate(['/blog']);
   }
 }
